cc6ffe60e7df160ee001a757ee922254d97b6de4
Admin <developer@ainiding.com>
2020-09-10 16:18:22 +0800

    202009101618

---
+++ cloud-eureka-server7002/src/main/java/com/myzzb/springcloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.myzzb.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author :zzb
+ * @createDate :2020/9/10 16:05
+ * @desc :
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
