9e36a358c91040091deb0015c8cc02040b73b5d5
zzb <17691228632@163.com>
2020-09-15 00:08:00 +0800

    202009132244

---
+++ cloud-config-client-3355/src/main/java/com/myzzb/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package com.myzzb.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author :zzb
+ * @createDate :2020/9/14 11:56 下午
+ * @desc :
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+}
