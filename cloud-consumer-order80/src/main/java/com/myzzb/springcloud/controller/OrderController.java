c54e3f1bb334fdc6ba5ce8741972b7c2b6e263e4
zzb <17691228632@163.com>
2020-09-15 22:50:51 +0800

    202009132244

--- cloud-consumer-order80/src/main/java/com/myzzb/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/myzzb/springcloud/controller/OrderController.java
@@ -20,7 +20,7 @@ import javax.annotation.Resource;
 public class OrderController {
     //private static final String PAYMENT_URL="http://localhost:8001";
     //通过在eureka上注册过的微服务名称调用
-    private static final String PAYMENT_URL = "http://CLOUD-PROVIDER-SERVICE";
+    private static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Resource
     private RestTemplate restTemplate;
@@ -33,5 +33,9 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    }
+    @GetMapping(value="/consumer/payment/zipkin")
+    public String paymentZipkin() {
+        return restTemplate.getForObject( PAYMENT_URL+"/payment/zipkin/",String.class);
     }
 }
